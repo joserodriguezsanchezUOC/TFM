0ed91b91301db0650ca19aca9207e98738eda56c
joserodriguezsanchezUOC <46453978+joserodriguezsanchezUOC@users.noreply.github.com>
2023-01-05 22:13:48 +0100

    Update app.py

--- FlaskWebScripts/app.py
+++ FlaskWebScripts/app.py
@@ -1,22 +1,20 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+app.config['SECRET_KEY'] = 'a0201893b879e3cff472febaf92f6260a98a1357200f225d'
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-    
 
-@app.route('/predict', methods=["POST"])
-def predict():
-	text = request.form.get("tituloOferta")
-	threshold = float(request.form.get("threshold"))
-	
-	"""
+# FUNCIÓN PRINCIPAL DE PREDICCIÓN
+def predictFunction(text, modelo, threshold):
+	# CARGA ETIQUETAS DE CONJUNTO DE DATOS
 	import pandas as pd
+	import csv
+	domain_labels = pd.read_csv('/home/iberanuncios/TFM/etiquetas/domainLabels_Nov2022.csv').values.tolist()
+	domain_labels = [i for row in domain_labels for i in row] # Flat
+	domain_labels = sorted(list(domain_labels))
+
 	import torch
 	import numpy as np
 	
@@ -25,78 +23,75 @@ def predict():
 	# Vectorización
 	from transformers import AutoTokenizer
 
-	# para multi etiquetas, se tiene que especificar el tipo de clasificación
+	# Se especifica el tipo de clasificación multietiqueta
 	tokenizer = AutoTokenizer.from_pretrained(model_ckpt, problem_type="multi_label_classification")
 	
+	# Se utiliza el modelo seleccionado
 	from transformers import AutoModelForSequenceClassification
-	model = AutoModelForSequenceClassification.from_pretrained("/home/iberanuncios/TFM/modelos/checkpointProduccion")
-
+	model = AutoModelForSequenceClassification.from_pretrained(modelo) # modelo elegible en formulario
+	
+	# Predicción
 	encoding = tokenizer(text, return_tensors="pt")
 	encoding = {k: v.to(model.device) for k,v in encoding.items()}
-
-	outputs = model(**encoding)
-	
+	outputs = model(**encoding)	
 	logits = outputs.logits
-
-	df = pd.read_csv('/home/iberanuncios/TFM/ofertasNov2022_preClean.csv')
-	
-	
-
-	from gensim.utils import deaccent
-	# Keywords a minúsculas y en un array
-	rows = []
-	for i, row in df.iterrows():
-	  # we sort the array
-	  labels = sorted([deaccent(item.strip().lower()) for item in row['label'].split(",")])
-	  rows.append({"text":row["text"],"labels": labels, "labels_str": ",".join(labels)})
-
-	df = pd.DataFrame(rows)
-	
-	domain_labels = set()
-	for row in df.itertuples():
-	  domain_labels.update(row.labels) # Los sets NO permiten elementos duplicados
-	domain_labels = sorted(list(domain_labels))
 	
 	sigmoid = torch.nn.Sigmoid()
 	probs = sigmoid(logits.squeeze().cpu())
 	predictions = np.zeros(probs.shape)
-	predictions[np.where(probs >= threshold)] = 1 # ajustar theshold
-	# turn predicted id's into actual label names
+	predictions[np.where(probs >= threshold)] = 1 # theshold ajustable en formulario
+	
+	# Convierte id en nombre de etiqueta
+	# Cambiada sintaxis de bucle por compatibilidad con Flask
 	predicted_labels=[]
 	for idx, label in enumerate(predictions):
 		if label == 1:
 			for idx2, label2 in enumerate(domain_labels):
 				if idx == idx2:
 					predicted_labels.append(domain_labels[idx2])
-					#print(domain_labels[idx2])
 	
-	return render_template('predict.html', prediction = predicted_labels, tituloOferta = text)
-	"""
-	return render_template('predict.html', prediction = "etiquetas", tituloOferta = text)
+	# SALIDA JSON PARA INTEGRACIÓN CON OTROS SERVICIOS
+	import json
+	json_str = json.dumps(predicted_labels)
+
+	return json_str
 
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route('/predictFromTitle', methods=["POST"])
-def predictFromTitle():
-	tituloOferta = request.form.get("tituloOferta")
+# PRECCIONES VÍA FORMULARIO WEB
+@app.route('/predict', methods=["POST"])
+def predict():
+	text = request.form.get("tituloOferta")
+	modelo = request.form.get("modelo")
 	
-	import torch
-	# Para ejecutar en CPU no CUDA
-	device = torch.device('cpu')
-	# Por defecto se aplica el modelo con Threshold= 0,5 (26824)
-	trainer = torch.load('/home/iberanuncios/TFM/modelos/checkpoint-26824/pytorch_model.bin',map_location=torch.device('cpu'))
+	if not modelo:
+		modelo="/home/iberanuncios/TFM/modelos/checkpointNov2022"
+	
+	threshold = request.form.get("threshold")
+	if not threshold:
+		threshold=0.5
+	else:
+		threshold = float(threshold)
+		
+	return render_template('predict.html', prediction = predictFunction(text, modelo, threshold), tituloOferta = text)
+	
 
-	# Probando el modelo
-	from transformers import pipeline
-	# Por defecto se aplica el modelo con Threshold= 0,5 (26824)
-	classifier = pipeline("text-classification",model='/home/iberanuncios/TFM/modelos/checkpoint-26824')
-	prediction = classifier(tituloOferta, )
+# PRECCIONES ORIENTADAS A WEB SERVICE (API)
+@app.route('/predictService', methods=["POST"])
+def predictService():
+	text = request.form.get("tituloOferta")
+	modelo = request.form.get("modelo")
 	
-	f1 = ""
+	if not modelo:
+		modelo="/home/iberanuncios/TFM/modelos/checkpointNov2022"
 	
-	for i in prediction:
-		f1 += str(i) +" "
-	
-	return f1
-	#render_template('predict.html', prediction = prediction)
-	
+	threshold = request.form.get("threshold")
+	if not threshold:
+		threshold=0.5
+	else:
+		threshold = float(threshold)
+		
+	return predictFunction(text, modelo, threshold)
